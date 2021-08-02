3e92fb46f63425e1ea6528f1a2f8dea148c1939c
Serdar Salci <serdarsalci@gmail.com>
2021-08-02 02:12:13 -0400

    first commit

---
+++ functions/index.js
@@ -0,0 +1,27 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+	// functions.logger.info("Hello there", { structuredData: true });
+	response.send("Hello from Firebase !");
+});
+
+exports.getScreams = functions.https.onRequest((req, res) => {
+	admin
+		.firestore()
+		.collection("screams")
+		.get()
+		.then(data => {
+			let screams = [];
+			data.forEach(doc => {
+				screams.push(doc.data());
+			});
+			return res.json(screams);
+		})
+		.catch(err => console.error(err));
+});
