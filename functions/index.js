f0094000521809d4832dafd46f937dcc3973ba20
Serdar Salci <serdarsalci@gmail.com>
2021-08-02 22:19:39 -0400

    Create & Read Data

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
 admin.initializeApp();
 
@@ -8,13 +8,13 @@ admin.initializeApp();
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
 	// functions.logger.info("Hello there", { structuredData: true });
-	response.send("Hello from Firebase !");
+	response.send('Hello from Firebase !');
 });
 
 exports.getScreams = functions.https.onRequest((req, res) => {
 	admin
 		.firestore()
-		.collection("screams")
+		.collection('screams')
 		.get()
 		.then(data => {
 			let screams = [];
@@ -24,4 +24,26 @@ exports.getScreams = functions.https.onRequest((req, res) => {
 			return res.json(screams);
 		})
 		.catch(err => console.error(err));
+});
+
+exports.createScream = functions.https.onRequest((req, res) => {
+	if (req.method !== 'POST') {
+		return res.status(400).json({ error: 'Method not allowed' });
+	}
+	const newScream = {
+		body: req.body.body,
+		userHandle: req.body.userHandle,
+		createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+	};
+	admin
+		.firestore()
+		.collection('screams')
+		.add(newScream)
+		.then(doc => {
+			res.json({ message: `document ${doc.id} created successfully` });
+		})
+		.catch(err => {
+			res.status(500).json({ error: `something went wrong` });
+			console.log(err.message);
+		});
 });
